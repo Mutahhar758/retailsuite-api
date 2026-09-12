c4ee52de563a27394c62b3c1d161c024ea837e46
Mutahhar Hanif <mutahharhanif@gmail.com>
2026-09-12 21:57:01 +0500

    feat: add CashPaid field to PurchaseMaster entity

---
+++ src/Migrators/Migrators.MSSQL/Migrations/Application/20260912165333_AddCashPaidToPurchaseMaster.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.MSSQL.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class AddCashPaidToPurchaseMaster : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "cash_back",
+                table: "PurchaseMaster",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "cash_paid",
+                table: "PurchaseMaster",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cash_back",
+                table: "PurchaseMaster");
+
+            migrationBuilder.DropColumn(
+                name: "cash_paid",
+                table: "PurchaseMaster");
+        }
+    }
+}
